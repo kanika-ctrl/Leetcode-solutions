578a1ebb78e1480ca9bf221143cad85cd2770544
kanika-ctrl <kanikamahiendra@gmail.com>
2026-01-16 20:13:50 +0530

    Time: 0 ms (100%), Space: 9.9 MB (5.96%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int,int>mymap;
+        for(int i=0;i<nums.size();i++){
+            mymap[nums[i]]++;
+        }
+        int ans=0;
+        for(auto it:mymap){
+          int freq=it.second;
+          if(freq>1){
+            ans+=(freq*(freq+1)/2)-freq;
+          }
+        }
+        return ans;
+    }
+};
