82d3c275ae915a34447d4d0ecddb814070fc40f9
Kanika <kanikamahiendra@gmail.com>
2026-06-19 17:46:49 +0530

    Time: 36 ms (14.27%), Space: 27.6 MB (7.28%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    void bfs(vector<vector<char>>& grid,map <pair<int,int>,bool> &mp,pair<int,int> p,int &ans){
-        int f=p.first;
-        int s=p.second;
+    void bfs(vector<vector<char>>& grid,int r,int c,int &ans){
+        grid[r][c]='0';
         int m=grid.size();
         int n=grid[0].size();
         vector<vector<int>> directions={{1,0},{-1,0},{0,1},{0,-1}};
-        mp[{f,s}]=1;
         queue<pair<int,int>> q;
-        q.push({f,s});
+        q.push({r,c});
         while(!q.empty()){
          pair<int,int> curr=q.front();
          q.pop();
@@ -17,9 +15,9 @@ public:
          for(auto &dir:directions){
             int new_i=i+dir[0];
             int new_j=j+dir[1];
-            if(new_i>=0 && new_i<m && new_j>=0 && new_j<n && grid[new_i][new_j]=='1'&&mp[{new_i,new_j}]==0){
+            if(new_i>=0 && new_i<m && new_j>=0 && new_j<n && grid[new_i][new_j]=='1'){
                 q.push({new_i,new_j});
-                mp[{new_i,new_j}]=1;
+                grid[new_i][new_j]='0';
             }
          }
         }
@@ -27,22 +25,14 @@ public:
         return;
     }
     int numIslands(vector<vector<char>>& grid) {
-        map<pair<int,int>,bool> mp;
+        int ans=0;
         int m=grid.size();
         int n=grid[0].size();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
-                    mp[{i,j}]=0;
+                    bfs(grid,i,j,ans);
                 }
-            }
-        }
-        int ans=0;
-        for(auto m:mp){
-            if(m.second==0){
-                int i=m.first.first;
-                int j=m.first.second;
-                bfs(grid,mp,{i,j},ans);
             }
         }
         return ans;
