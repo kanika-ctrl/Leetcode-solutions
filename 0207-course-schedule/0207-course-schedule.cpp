fb38f7865faee3a79d29438de70722294b115cf4
Kanika <kanikamahiendra@gmail.com>
2026-06-22 15:36:26 +0530

    Time: 1 ms (82.55%), Space: 19.1 MB (81.3%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
-bool iscycle(vector<vector<int>>& adj,vector<bool> &visited,vector<bool> &inrec,int u){
-    visited[u]=1;
-    inrec[u]=1;
-    for(int i=0;i<adj[u].size();i++){
-        if(!visited[adj[u][i]]&&iscycle(adj,visited,inrec,adj[u][i])){
-            return true;
-        }
-        else if (inrec[adj[u][i]]){
-           return true;
-        } 
-    }
-    inrec[u]=0;
-    return false;
-}
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        for(int i=0;i<prerequisites.size();i++){
-            int courses=prerequisites[i][0];
-            int prereq=prerequisites[i][1];
-            adj[prereq].push_back(courses);
+        for(auto &edge: prerequisites){
+            int u=edge[1];
+            int v=edge[0];
+            adj[u].push_back(v);
         }
-        vector<bool> visited(numCourses,0);
-        vector<bool> inrec(numCourses,0);
+        vector<int> indegree(numCourses,0);
+        queue<int> q;
         for(int i=0;i<numCourses;i++){
-            if(!visited[i]&&iscycle(adj,visited,inrec,i)){
-                return false;
+            for(int j=0;j<adj[i].size();j++){
+                indegree[adj[i][j]]++;
             }
         }
-        return true;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        int processed=0;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            processed++;
+            for(int i=0;i<adj[node].size();i++){
+              indegree[adj[node][i]]--;
+              if(indegree[adj[node][i]]==0){
+                q.push(adj[node][i]);
+              }
+            }
+        }
+        if(processed==numCourses){
+            return true;
+        }
+        return false;
     }
 };
