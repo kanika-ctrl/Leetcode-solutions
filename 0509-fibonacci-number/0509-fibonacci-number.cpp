1a987c904ed4839d400007198c3a87885f464fe5
Kanika <kanikamahiendra@gmail.com>
2026-07-23 17:35:12 +0530

    Time: 3 ms (65.27%), Space: 8.2 MB (16.83%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n,vector<int> &dp){
+        if(n<=1){
+            return n;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=fib(n-1,dp)+fib(n-2,dp);
+    }
+    int fib(int n) {
+        //memoziation part
+        vector<int>dp(n+1,-1);
+       int ans= fib(n,dp);
+       return ans;
+    }
+};
