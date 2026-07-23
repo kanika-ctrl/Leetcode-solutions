8ce395d913245af0bcea5f86ca948ed289995217
Kanika <kanikamahiendra@gmail.com>
2026-07-23 17:40:02 +0530

    Time: 0 ms (100%), Space: 7.8 MB (38.73%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -12,13 +12,24 @@ public:
     // }
     int fib(int n) {
         //TABULATION METHOD OR BOTTOM UP
-        if(n<=1) return n;
-        vector<int>dp(n+1);
-        dp[0]=0;
-        dp[1]=1;
-       for(int i=2;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
-       }
-       return dp[n];
+    //     if(n<=1) return n;
+    //     vector<int>dp(n+1);
+    //     dp[0]=0;
+    //     dp[1]=1;
+    //    for(int i=2;i<=n;i++){
+    //     dp[i]=dp[i-1]+dp[i-2];
+    //    }
+    //    return dp[n];
+    //SPACE OPTIMIZATION IN BOTTOM UP
+    if(n<=1) return n;
+    vector<int> dp(3);
+    dp[0]=0;
+    dp[1]=1;
+    for(int i=2;i<=n;i++){
+        dp[2]=dp[0]+dp[1];
+        dp[0]=dp[1];
+        dp[1]=dp[2];
+    }
+    return dp[2];
     }
 };
